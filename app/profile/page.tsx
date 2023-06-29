f9e029be52fc9091ab414f771ac3435cea36bcdd
shawnbirring <shawnbirring21@gmail.com>
2023-06-29 16:39:21 -0700

    mobile friendly profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,12 +7,12 @@ export default function Resume() {
   return (
     <>
       <DownloadResume />
-      <div className="grid min-h-screen grid-cols-2 gap-5 p-5 text-white">
-        <div className="space-y-5">
+      <div className="grid min-h-screen grid-cols-1 gap-5 p-5 text-white md:grid-cols-2">
+        <div className="space-y-5 md:col-span-1">
           <Skills />
           <Education />
         </div>
-        <div className="col-span-2 space-y-5 md:col-span-1">
+        <div className="space-y-5 md:col-span-1">
           <Experience />
         </div>
       </div>
